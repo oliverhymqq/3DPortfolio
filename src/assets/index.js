e8198b0a4ab61fa2dcfd73b00218fb878bd5d865
oliverhymqq <101374173+oliverhymqq@users.noreply.github.com>
2023-05-21 18:56:28 +1000

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg'
+import backend from './backend.png'
+import creator from './creator.png'
+import mobile from './mobile.png'
+import web from './web.png'
+import github from './github.png'
+import menu from './menu.svg'
+import close from './close.svg'
+
+import css from './tech/css.png'
+import docker from './tech/docker.png'
+import figma from './tech/figma.png'
+import git from './tech/git.png'
+import html from './tech/html.png'
+import javascript from './tech/javascript.png'
+import mongodb from './tech/mongodb.png'
+import nodejs from './tech/nodejs.png'
+import reactjs from './tech/reactjs.png'
+import redux from './tech/redux.png'
+import reactrouter from './tech/reactrouter.png'
+import bootstrap from './tech/bootstrap.png'
+import tailwind from './tech/tailwind.png'
+import typescript from './tech/typescript.png'
+import threejs from './tech/threejs.svg'
+
+import hp from './company/hp.png'
+import huawei from './company/huawei.png'
+import ozmas from './company/ozmas.png'
+import GO from './company/GO.png'
+import udemy from './company/udemy.png'
+
+import cocktailapp from './cocktailapp.png'
+import hmostore from './hmostore.png'
+import unsplash from './unsplash.png'
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  reactrouter,
+  bootstrap,
+  tailwind,
+  typescript,
+  threejs,
+  hp,
+  huawei,
+  ozmas,
+  GO,
+  udemy,
+  cocktailapp,
+  unsplash,
+  hmostore,
+}
